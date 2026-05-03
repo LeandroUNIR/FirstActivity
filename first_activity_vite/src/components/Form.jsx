0086bf8361c33e28c9a6a424b5c88d3bc4e21b59
LeandroUNIR <josephleandro.t4770@comunidadunir.net>
2026-05-03 13:35:54 -0500

    Título catálogo, className en Form

--- first_activity_vite/src/components/Form.jsx
+++ first_activity_vite/src/components/Form.jsx
@@ -3,19 +3,19 @@ import Input from "./Input";
 const Form = () => {
     return (
     <form>
-        <div class="grid gap-6 mb-6 md:grid-cols-2">
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
             <Input></Input>
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
 
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
 
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
 
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
             
         </div>
         <Button color="blue" type="submit">Submit</Button>
--- first_activity_vite/src/pages/Catalog/Catalog.jsx
+++ first_activity_vite/src/pages/Catalog/Catalog.jsx
@@ -4,13 +4,16 @@ import Books from '../../data/books.json';
 const Catalog = () => {
     return (
         <div className="flex flex-wrap flex-row gap-10 p-6 max-w-5xl mx-auto">
-            {
-                Books.map((book,i) => (
-                    <div className="basis-64" key={i}>
-                        <Card title={book.titulo} description={book.descripcion} imageRoute="/prueba3.jpg" />
-                    </div>
-                ))
-            }
+            <h1 className="text-3xl font-bold text-left">Libros más populares</h1>
+            <div className="flex flex-wrap flex-row gap-10 p-6 max-w-5xl mx-auto">
+                {
+                    Books.map((book, i) => (
+                        <div className="basis-64" key={i}>
+                            <Card title={book.titulo} description={book.descripcion} imageRoute="/prueba3.jpg" />
+                        </div>
+                    ))
+                }
+            </div>
             {/* <div class="basis-full">01</div>
             <div class="basis-full">02</div>
             <div class="basis-full">03</div> */}
