739e8fd27e102085246b8284af2fbff92e68660f
LeandroUNIR <josephleandro.t4770@comunidadunir.net>
2026-05-02 22:56:12 -0500

    Carga de archivo json en catalogo

--- first_activity_vite/src/pages/Catalog/Catalog.jsx
+++ first_activity_vite/src/pages/Catalog/Catalog.jsx
@@ -1,6 +1,20 @@
+import Books from '../../data/books.json';
+
 const Catalog = () => {
     return (
-        <h2>Catálogo</h2>
+        <div class="flex flex-row gap-10 p-6 max-w-5xl mx-auto">
+            {
+                Books.map((book,i) => (
+                    <div className="basis-full" key={i}>
+                        <h3>{book.titulo}</h3>
+                        <p>{book.autor}</p>
+                    </div>
+                ))
+            }
+            {/* <div class="basis-full">01</div>
+            <div class="basis-full">02</div>
+            <div class="basis-full">03</div> */}
+        </div>
     );
 }
 
