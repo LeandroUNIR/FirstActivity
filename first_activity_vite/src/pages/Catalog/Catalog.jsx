ea23632f54b05769d2a18736e91560a216b2e3b5
LeandroUNIR <josephleandro.t4770@comunidadunir.net>
2026-05-03 22:19:11 -0500

    Hook useContext, modificación card, detalle libro

--- first_activity_vite/src/pages/Catalog/Catalog.jsx
+++ first_activity_vite/src/pages/Catalog/Catalog.jsx
@@ -2,21 +2,19 @@ import Card from '../../components/Card';
 import Books from '../../data/books.json';
 
 const Catalog = () => {
+    
     return (
-        <div className="flex flex-wrap flex-row gap-10 p-6 max-w-5xl mx-auto">
+        <div >
             <h1 className="text-3xl font-bold text-left">Libros más populares</h1>
             <div className="flex flex-wrap flex-row gap-10 p-6 max-w-5xl mx-auto">
                 {
                     Books.map((book, i) => (
-                        <div className="basis-64" key={i}>
-                            <Card title={book.titulo} description={book.descripcion} imageRoute="/prueba3.jpg" />
+                        <div className="basis-64 text-center" key={i}>
+                            <Card title={book.titulo} description={book.costo_cop} imageRoute="/prueba3.jpg" bookId={book.id}/>
                         </div>
                     ))
                 }
             </div>
-            {/* <div class="basis-full">01</div>
-            <div class="basis-full">02</div>
-            <div class="basis-full">03</div> */}
         </div>
     );
 }
--- first_activity_vite/src/router/AppRouter.jsx
+++ first_activity_vite/src/router/AppRouter.jsx
@@ -9,31 +9,33 @@ import Landing from "../pages/Landing/Landing.jsx";
 import Cart from "../pages/Cart/Cart.jsx";
 // import Login from "../pages/Login/Login.jsx";
 // import Home from "../pages/Home/Home.jsx";
- import Catalog from "../pages/Catalog/Catalog.jsx";
-// import BookDetail from "../pages/BookDetail/BookDetail.jsx";
+import Catalog from "../pages/Catalog/Catalog.jsx";
+import BookDetail from "../pages/BookDetail/BookDetail.jsx";
 // import Checkout from "../pages/Checkout/Checkout.jsx";
 import Profile from "../pages/Profile/Profile.jsx";
+import { GlobalProvider } from "../context/AuthContext/global/GlobalProvider.jsx";
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
-
-            <Routes>
-                {/* Rutas desactivadas temporalmente */}
-                <Route  element={<Layout />}>
-                    <Route path="/" element={<Landing />} />
-                    <Route path="catalog" element={<Catalog />} />
-                    <Route path="cart" element={<Cart />} />
-                    {/* <Route path="login" element={<Login />} />
+            <GlobalProvider>
+                <Routes>
+                    {/* Rutas desactivadas temporalmente */}
+                    <Route element={<Layout />}>
+                        <Route path="/" element={<Landing />} />
+                        <Route path="catalog" element={<Catalog />} />
+                        <Route path="cart" element={<Cart />} />
+                        <Route path="bookDetail" element={<BookDetail />} />
+                        {/* <Route path="login" element={<Login />} />
                     <Route path="home" element={<Home />} />
+                    <Route path="checkout" element={<Checkout />} />*/}
+                        <Route path="profile" element={<Profile />} />
+                        <Route path="catalog/bookDetail" element={<BookDetail />} />
 
-                    <Route path="book/:id" element={<BookDetail />} />
-                    <Route path="checkout" element={<Checkout />} />*/
-                    <Route path="profile" element={<Profile />} /> }
-                </Route>
+                    </Route>
 
-            </Routes>
-
+                </Routes>
+            </GlobalProvider>
         </BrowserRouter>
     );
 };
